db7160f8895912c90d49494c58fb8b6198d4569a
stoneG <me@sitong>
2012-11-27 14:59:50 -0500

    built everything but the server request responder

--- socket/TCPserver.py
+++ socket/TCPserver.py
@@ -7,7 +7,7 @@ class Server(object):
         self.host = host
         self.port = port
         ports = [80, 8080, 'end']
-        self.automatic_ports = [p for p in ports is p != port]
+        self.automatic_ports = [p for p in ports if p != port]
 
     def run(self):
         """Binds socket to self.port or one of the self.automatic_ports.
@@ -18,21 +18,45 @@ class Server(object):
             try:
                 print 'Running server on %s:%d' % (self.host, self.port)
                 self.socket.bind((self.host, self.port))
-                print 'Now listening on', s.getsockname()
-                self.listen()
-                return
+                print 'Now listening on', self.socket.getsockname()
             except Exception as e:
                 print 'Cannot bind to port', self.port
                 print 'Trying on a new port...'
                 self.port = self.automatic_ports[0]
                 del self.automatic_ports[0]
                 continue
+            self.listen()
+            return
         print 'None of the ports worked :('
 
     def listen(self):
         """Listen for requests on a TCP, IPv4 Socket."""
         while True:
-            pass
+            print 'Waiting for connection'
+            self.socket.listen(1)
+            conn, addr = self.socket.accept()
+            print 'Accepted connection from:', addr
 
-    def respond(self, requestType):
-        """Sends browser HTTP requests."""
+            msg = conn.recv(1024)
+            request = msg.split()[0]
+            print 'HTTP Request:', request
+
+            self.respond(request, conn)
+
+    def respond(self, requestType, conn):
+        """Responds to HTTP requests."""
+        file_requested = string.split(' ')[1]
+        print file_requested
+
+
+def main():
+    if len(sys.argv) == 3:
+        server = Server(sys.argv[1], sys.argv[2])
+    elif len(sys.argv) == 2:
+        server = Server(sys.argv[1])
+    else:
+        server = Server()
+    server.run()
+
+if __name__ == '__main__':
+    main()
